5f9918491fd8e72df19c7091e4a97f82e4a41aac
evgenymartinov <46422284+evgenymartinov@users.noreply.github.com>
2019-07-19 18:06:01 +0300

    Update itemJS.js

--- Kinarti/Kinarti/PriceIT/itemJS.js
+++ Kinarti/Kinarti/PriceIT/itemJS.js
@@ -821,19 +821,9 @@ function success(data) {
                     { data: "ID" },
                     { data: "Name" },
                     {
-                        //data: "BoxMeasuresID"
+                        //data: "BoxMeasuresID" ככ
 
                             render: function (data, type, row, meta) {
-                                //let BoxMeasures = "data-itemId='" + row.ID + "'";
-
-                                //for (var j = 0; j < myBoxes.length; j++) {
-                                //    if (myBoxes.ID === row.BoxMeasuresID) {
-                                //        return myBoxes[j].Height + 'X' + myBoxes[j].Width + 'X' + myBoxes[j].Depth
-                                //    }
-
-                            //}
-
-
                             let theRightBoxMeasures = myBoxes.find(function (item) {
                                 console.log(item);
                                 return item.ID === row.BoxMeasuresID;
@@ -842,7 +832,7 @@ function success(data) {
                                 return theRightBoxMeasures.Height + 'X' + theRightBoxMeasures.Width + 'X' + theRightBoxMeasures.Depth;
                             }
 
-                            } 
+                            }
 
                     },  
                     { data: "Cost" },
